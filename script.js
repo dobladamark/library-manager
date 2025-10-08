c2413fc13e1f20ddacb52dac010c0ecfacbfaa07
mark <122426238+dobladamark@users.noreply.github.com>
2025-10-08 13:41:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-const library = [];
+const library = [ {
+    title: 'Your Next Five Moves: Master the Art of Business Strategy',
+    author: 'Patrick Bet-David and Greg Dinkin',
+    about: 'A book on how to plan ahead',
+    pages: 320,
+  },
+                 
+];
 
